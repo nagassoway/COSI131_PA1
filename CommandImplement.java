0580ba6966d16f9c22729e3834b89f1a583a5291
nagassoway <33837594+nagassoway@users.noreply.github.com>
2018-09-30 11:35:41 -0400

    Update CommandImplement.java

--- CommandImplement.java
+++ CommandImplement.java
@@ -1,3 +1,39 @@
 public class CommandImplement extends SequentialFilter {
 
+    public void cat() {
+      
+    }
+  
+    public void ls() {
+      
+    }
+  
+    public void cd() {
+      
+    }
+  
+    public void pwd() {
+      
+    }
+  
+    public void grep() {
+      
+    }
+  
+    public void wc() {
+      
+    }
+  
+    public void uniq() {
+      
+    }
+  
+    public void greaterThan() {
+
+    }
+  
+    @Overide
+    public void processLine() {
+      
+    }
 }
