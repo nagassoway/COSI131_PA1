ea3ec9d18db7f427ce3c64493339c65bfc7931b7
nagassoway <33837594+nagassoway@users.noreply.github.com>
2018-09-30 13:50:29 -0400

    Update SequentialCommandBuilder.java

--- SequentialCommandBuilder.java
+++ SequentialCommandBuilder.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 public class SequentialCommandBuilder {
 	private static List<SequentialFilter> filters;
+	protected static SequentialBuilder commands;
 	
 	public static List<SequentialFilter> createFiltersFromCommand(String command){
+		commands = new CommandInterpreter();
+		commands.input = new LinkedList();
 		filters = new LinkedList<SequentialFilter>();
 		String[] segments = command.split("[|]");
 		for (String i: segments) {
@@ -56,6 +59,31 @@ public class SequentialCommandBuilder {
 			System.out.println("The command " + subCommand + " was not recognized.");
 		}
 		return null;
+	}
+	
+	// Nick's version
+	private static SequentialFilter constructFilterFromSubCommand(String subCommand){
+		String[] segments = subCommand.split(" ");
+		if (checkCommands(segments[0]) == false) {
+			return null;			
+		} else {
+			commands.input.add(subCommand);
+			return null;
+		}
+	}
+
+	private static boolean checkCommands(String subCommand) {
+		switch (subCommand) {
+		case "pwd" : return true;
+		case "ls" : return true;
+		case "cd" : return true;
+		case "cat" : return true;
+		case "grep" : return true;
+		case "wc" : return true;
+		case "uniq" : return true;
+		case "exit": return true;
+		default: System.out.println(Message.COMMAND_NOT_FOUND); return false;
+		}
 	}
 
 	private static boolean linkFilters(List<SequentialFilter> filters){
