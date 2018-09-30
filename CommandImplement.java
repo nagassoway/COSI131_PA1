d42310417d7e85683afead534abfd4dad1916e26
mnkhayter <35772716+mnkhayter@users.noreply.github.com>
2018-09-30 12:11:01 -0400

    cd start

--- CommandImplement.java
+++ CommandImplement.java
@@ -1,5 +1,34 @@
+import java.io.*;
+
 public class CommandImplement extends SequentialFilter {
 
+    	public String processFile(String file) {
+		
+		File f = new File(file);
+		
+		try {
+			Scanner sc = new Scanner(f);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.print("At least one of the files in the command" + "was not found.\n");
+					
+		}
+	
+		
+		return null;
+	}
+	
+	public String cd(String newPath) {
+	    if (isAbsolutePath(newPath)) {
+	        path = normalizePath(newPath);
+	        return this;
+	    }
+
+	    path = normalizePath(path + SEPARATOR + newPath);
+	    return this;
+	}
+    
     public void cat() {
       
     }
