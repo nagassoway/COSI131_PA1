2e9096481ba2cdede623578d9482675bdc6a1757
mnkhayter <35772716+mnkhayter@users.noreply.github.com>
2018-09-30 19:59:45 -0400

    Updated REPL

--- SequentialREPL.java
+++ SequentialREPL.java
@@ -17,18 +17,14 @@ public class SequentialREPL {
 		Scanner input = new Scanner(System.in);
 		String cmd = input.nextLine();
 		List<SequentialFilter> cmds = new LinkedList<SequentialFilter>();
-		String[] splits = new String[10];
 		while(!cmd.equals("exit")) {
 			
-			cmds.clear();
 			System.out.print(Message.NEWCOMMAND);
-			cmd = input.nextLine();
 			//Create linkedlist of commands into String lines of CommandImplements.
 			cmds = SequentialCommandBuilder.createFiltersFromCommand(cmd);
-			//SequentialCommandBuilder.linkFilters(cmds);
-			//Iterator<SequentialFilter> it = cmds.iterator();
-			//CommandImplement next = (CommandImplement) it.next();
+			
 			System.out.println();
+			cmd = input.nextLine();
 		}
 		System.out.print(Message.GOODBYE);
 	}
