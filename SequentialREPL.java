0434c70bd3f30bf9a1080807b50c397d746cb66f
mnkhayter <35772716+mnkhayter@users.noreply.github.com>
2018-09-30 19:41:23 -0400

    edited repl

--- SequentialREPL.java
+++ SequentialREPL.java
@@ -1,13 +1,14 @@
 package cs131.pa1.filter.sequential;
+
 import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.*;
+import cs131.pa1.filter.Message;
 
 public class SequentialREPL {
 
 	static String currentWorkingDirectory;
 	
-	@SuppressWarnings("resource")
 	@SuppressWarnings("resource")
 	public static void main(String[] args){
 		
@@ -16,26 +17,18 @@ public class SequentialREPL {
 		Scanner input = new Scanner(System.in);
 		String cmd = input.nextLine();
 		List<SequentialFilter> cmds = new LinkedList<SequentialFilter>();
+		String[] splits = new String[10];
 		while(!cmd.equals("exit")) {
 			
+			cmds.clear();
 			System.out.print(Message.NEWCOMMAND);
 			cmd = input.nextLine();
-			cmds.clear();
 			//Create linkedlist of commands into String lines of CommandImplements.
 			cmds = SequentialCommandBuilder.createFiltersFromCommand(cmd);
-			Iterator<SequentialFilter> it = cmds.iterator();
-			CommandImplement next = (CommandImplement) it.next();
-			CommandImplement prev = next;
-			next.process();
-			while(it.hasNext()) {
-				
-				next = (CommandImplement) it.next();
-				next.process();
-
-			}
-			System.out.println(next.output.poll());
-			
-			
+			//SequentialCommandBuilder.linkFilters(cmds);
+			//Iterator<SequentialFilter> it = cmds.iterator();
+			//CommandImplement next = (CommandImplement) it.next();
+			System.out.println();
 		}
 		System.out.print(Message.GOODBYE);
 	}
