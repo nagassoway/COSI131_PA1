7da0b173708a082af116a5b1c4ba74e1444387da
nagassoway <33837594+nagassoway@users.noreply.github.com>
2018-09-30 12:46:39 -0400

    Splits the commands

--- SequentialCommandBuilder.java
+++ SequentialCommandBuilder.java
@@ -3,9 +3,16 @@ package cs131.pa1.filter.sequential;
 import java.util.List;
 
 public class SequentialCommandBuilder {
+	private static List<SequentialFilter> filters;
 	
 	public static List<SequentialFilter> createFiltersFromCommand(String command){
-		return null;
+		filters = new LinkedList<SequentialFilter>();
+		String[] segments = command.split("[|]");
+		for (String i: segments) {
+			i = i.trim();
+			filters.add(constructFilterFromSubCommand(i));
+		}
+		return filters;
 	}
 	
 	private static SequentialFilter determineFinalFilter(String command){
