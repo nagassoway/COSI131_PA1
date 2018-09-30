f39c2a9b5651228de2a5dbb9afa3a1dc733560d1
mnkhayter <35772716+mnkhayter@users.noreply.github.com>
2018-09-30 12:09:12 -0400

    Parsing If Statements

--- SequentialCommandBuilder.java
+++ SequentialCommandBuilder.java
@@ -5,6 +5,43 @@ import java.util.List;
 public class SequentialCommandBuilder {
 	
 	public static List<SequentialFilter> createFiltersFromCommand(String command){
+		
+		String[] splits = command.split("|", -1);
+		for(int i = 0; i < splits.length; i++) {
+			
+			if(command.substring(0, 2).equals("cd")) {
+			
+			
+			}
+			else if(command.substring(0,3).equals("cat")) {
+			
+			
+			}
+			else if(command.substring(0, 2).equals("ls")) {
+			
+			
+			}
+			else if(command.substring(0, 3).equals("pwd")) {
+			
+			
+			}
+			else if(command.substring(0,4).equals("grep")) {
+			
+			
+			}
+			else if(command.substring(0, 2).equals("wc")) {
+			
+			
+			}
+			else if(command.substring(0, 4).equals("uniq")) {
+			
+			
+			}
+			else {
+			
+				System.out.println("The command " + splits[i] + " was not recognized.");
+			}
+		}
 		return null;
 	}
 	
