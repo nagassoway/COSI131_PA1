e6345c4903e36f18b6a05eebe13f7b74963f61fb
mnkhayter <35772716+mnkhayter@users.noreply.github.com>
2018-09-30 20:01:40 -0400

    -ln

--- SequentialREPL.java
+++ SequentialREPL.java
@@ -12,7 +12,7 @@ public class SequentialREPL {
 	@SuppressWarnings("resource")
 	public static void main(String[] args){
 		
-		System.out.println(Message.WELCOME);
+		System.out.print(Message.WELCOME);
 		System.out.print(Message.NEWCOMMAND);
 		Scanner input = new Scanner(System.in);
 		String cmd = input.nextLine();
